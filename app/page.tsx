4167c0719cc3bc0afd2f7623db9066f8e8596e6d
Domas <domukas1337@gmail.com>
2025-06-22 17:02:22 +0300

    homepage finished, margin changes, navbar changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen mx-10 sm:mx-0">
-      <h1 className="text-4xl mb-2">
-        Welcome to <span className="text-blue-400">DMKAS</span>
-      </h1>
-      <p className="text-lg sm:text-2xl text-center font-semibold text-gray-200">
-        A website for searcing manga and anime
-      </p>
-      <p className="text-base text-center">
-        Try searching for your favorite manga or anime by hovering the{" "}
-        <span className="font-bold">search </span>
-        button
-      </p>
+    <div className="flex flex-col lg:flex-row justify-center items-center h-screen mx-10 lg:mx-0 gap-10 mt-14 sm:mt-0">
+      <div className="flex flex-col">
+        <h1 className="text-4xl mb-2">
+          Welcome to <span className="text-blue-400">DMKAS</span>
+        </h1>
+        <p className="text-lg sm:text-2xl font-semibold text-gray-200">
+          A website for searcing manga and anime
+        </p>
+        <p className="text-base">
+          Try searching for your favorite manga or anime by hovering the{" "}
+          <span className="font-bold">search </span>
+          button
+        </p>
+      </div>
+      <div className="flex flex-col">
+        <Image src="/homepage.png" alt="homepage" width={400} height={500} />
+        <Link
+          href="https://x.com/morinohitos"
+          className="text-center hover:text-blue-400 transition-colors"
+        >
+          @morinohitos
+        </Link>
+      </div>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import { FaBook } from "react-icons/fa";
 export default function Navbar() {
   return (
     <div className="fixed top-0 left-0 right-0 bg-gray-700/50 backdrop-blur-lg">
-      <div className="flex justify-between items-center p-4 mx-24">
+      <div className="flex justify-between items-center p-4 mx-0 md:mx-24">
         <Link
           href="/"
           className="text-2xl hover:text-blue-400 transition-colors duration-150 barlow font-black"
@@ -15,9 +15,12 @@ export default function Navbar() {
         </Link>
         <div className="flex gap-8">
           <div className="relative group">
-            <span className="cursor-pointer hover:text-blue-400 transition-colors duration-150">
+            <Link
+              href="/search"
+              className="cursor-pointer hover:text-blue-400 transition-colors duration-150"
+            >
               Search
-            </span>
+            </Link>
             <div className="absolute top-full left-1/2 transform -translate-x-1/2 px-3 py-2 bg-gray-800 text-white text-sm rounded-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-300 whitespace-nowrap">
               <div className="flex flex-col">
                 <Link
@@ -77,7 +80,7 @@ export default function Navbar() {
             </div>
           </div>
           <Link
-            href="about:blank"
+            href="https://github.com/domukas1337"
             className="hover:cursor-pointer hover:text-blue-400 transition-colors duration-300"
           >
             Github
