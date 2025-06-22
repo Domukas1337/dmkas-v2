d9b3167472e831cc064bc07d8a64275710abf5e3
Domas <domukas1337@gmail.com>
2025-06-22 11:56:56 +0300

    new font, margins

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,19 +9,19 @@ export default function Navbar() {
       <div className="flex justify-between items-center p-4 mx-24">
         <Link
           href="/"
-          className="text-2xl hover:text-blue-400 transition-colors duration-300"
+          className="text-2xl hover:text-blue-400 transition-colors duration-150 barlow font-black"
         >
           DMKAS
         </Link>
         <div className="flex gap-8">
           <div className="relative group">
-            <span className="cursor-pointer hover:text-blue-400 transition-colors duration-300">
+            <span className="cursor-pointer hover:text-blue-400 transition-colors duration-150">
               Search
             </span>
-            <div className="absolute top-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 bg-gray-800 text-white text-sm rounded-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-300 whitespace-nowrap">
+            <div className="absolute top-full left-1/2 transform -translate-x-1/2 px-3 py-2 bg-gray-800 text-white text-sm rounded-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-300 whitespace-nowrap">
               <Link
                 href="about:blank"
-                className="flex items-center gap-3.5 hover:cursor-pointer text-gray-400 hover:text-white transition-colors duration-300 mx-2"
+                className="flex items-center gap-3.5 hover:cursor-pointer text-gray-400 hover:text-white transition-colors duration-150 mx-2"
               >
                 <FaPlay />
                 <span className="text-base font-black">Anime</span>
