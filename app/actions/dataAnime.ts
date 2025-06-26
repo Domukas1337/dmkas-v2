a61e5af778c0014b705b3f787a15c3b7977c10ca
Domas <domukas1337@gmail.com>
2025-06-26 23:46:15 +0300

    anime fetching done

---
+++ app/actions/dataAnime.ts
@@ -0,0 +1,27 @@
+export async function getAnime({
+  search,
+  genre,
+  year,
+  status,
+}: {
+  search: string;
+  genre: number;
+  year: string;
+  status: string;
+}) {
+  if (!year) {
+    const res = await fetch(
+      `https://api.jikan.moe/v4/anime?q=${search}&genres=${genre}&status=${status}`
+    );
+
+    const data = await res.json();
+    return data;
+  } else {
+    const res = await fetch(
+      `https://api.jikan.moe/v4/anime?q=${search}&genres=${genre}&start_date=${`${year}-01-01`}&end_date=${`${year}-12-31`}&status=${status}`
+    );
+
+    const data = await res.json();
+    return data;
+  }
+}
--- app/search/anime/page.tsx
+++ app/search/anime/page.tsx
@@ -1,6 +1,29 @@
+"use client";
+import { getAnime } from "@/app/actions/dataAnime";
 import SearchAnime from "@/components/Search";
 
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Anime() {
+  const params = useSearchParams();
+  const search = params.get("q") || "";
+  const genre = Number(params.get("genre")) || 0;
+  const year = params.get("year") || "";
+  const status = params.get("status") || "";
+
+  useEffect(() => {
+    async function fetch() {
+      if (!search) {
+        return;
+      }
+      const data = await getAnime({ search, genre, year, status });
+      console.log(data);
+      return data;
+    }
+
+    fetch();
+  }, [search, genre, year, status]);
   return (
     <div className="flex justify-center items-center h-screen">
       <SearchAnime />
