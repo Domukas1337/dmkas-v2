ddfc750a09bc5e0763b9bc8e9875e400543a0e42
Domas <domukas1337@gmail.com>
2025-09-12 19:58:21 +0300

    removed /test directory

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoadingCard from "@/components/LoadingCard";
-
-export default function Test() {
-  return (
-    <div className="flex flex-col justify-center items-center mt-20 w-screen">
-      <LoadingCard />
-    </div>
-  );
-}
